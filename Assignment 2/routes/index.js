4db68214aa0b1b699568b8b9e00b8e996432904b
Alex Supkay <asupkay1124@gmail.com>
2018-06-10 13:25:52 -0400

    Edit layout

--- Assignment 2/routes/index.js
+++ Assignment 2/routes/index.js
@@ -1,6 +1,21 @@
 const constructorMethod = app => {
   app.get('/', (req, res) => {
-    res.render('portfolio/homepage');
+    res.render('portfolio/homepage', {products: [
+      {
+        id: 1,
+        name: "Wonderball",
+        img: "/public/img/wonderball.jpg",
+        description: "It was chocolate",
+        details: "It was milk chocolate",
+      },
+      {
+        id: 2,
+        name: "Notball",
+        img: "/public/img/notball.jpg",
+        description: "It was not chocolate",
+        details: "It was not milk chocolate",
+      }
+    ]});
   }); 
 
   app.use('*', (req,res) => {
